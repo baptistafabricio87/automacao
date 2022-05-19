a9105b08ba48dedc56d23e1a7c926d61ae4376dc
Fabricio Castro <baptistafabricio87@gmail.com>
2022-05-19 15:02:38 -0300

    Adição de requirements e correção de estilo do codigo

---
+++ extracao_contratos/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='extracao_contratos',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Fabricio Castro',
+    author_email='baptistafabricio87@gmail.com',
+    description=''
+)
